9ce76b658ce790d7a84283b018fcca00718a9a58
thrishaliniTGM <trishalini@thegermanemedia.com>
2024-02-23 01:55:31 +0530

    added get and delete operations

---
+++ src/delete.js
@@ -0,0 +1,30 @@
+const AWS = require("aws-sdk");
+const s3 = new AWS.S3();
+
+const BUCKET_NAME = process.env.FILE_UPLOAD_BUCKET_NAME;
+
+module.exports.handler =async(event) =>{
+    console.log(event);
+    const response = {
+        isBAse64Encoded: false,
+        statusCode :200,
+    };
+
+    try{
+        const params = {
+            Bucket: BUCKET_NAME,
+            Key: decodeURIComponent(event.pathParameters.fileKey),
+
+        };
+        const deleteResult = await s3.deleteObject(params).promise();
+        response.body = JSON.stringify({message:"Successfully deleted file from s3", deleteResult});
+        
+    }
+    catch(e){
+        console.error(e);
+        response.body = JSON.stringify({message:"Error in deleting from s3", errorMessage : e });
+        response.status = 500;
+    }
+
+    return response;
+};
---
+++ src/get.js
@@ -0,0 +1,30 @@
+const AWS = require("aws-sdk");
+const s3 = new AWS.S3();
+
+const BUCKET_NAME = process.env.FILE_UPLOAD_BUCKET_NAME;
+
+module.exports.handler =async(event) =>{
+    console.log(event);
+    const response = {
+        isBAse64Encoded: false,
+        statusCode :200,
+    };
+
+    try{
+        const params = {
+            Bucket: BUCKET_NAME,
+            Key: decodeURIComponent(event.pathParameters.fileKey),
+
+        };
+        const getResult = await s3.getObject(params).promise();
+        response.body = JSON.stringify({message:"Successfully retrived file from s3",getResult});
+        
+    }
+    catch(e){
+        console.error(e);
+        response.body = JSON.stringify({message:"Error in file retrival from s3", errorMessage : e });
+        response.status = 500;
+    }
+
+    return response;
+};

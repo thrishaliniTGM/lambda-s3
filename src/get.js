cbfa6a0ccee1ccc67d37cf6d2b77ca67dc02c257
thrishaliniTGM <trishalini@thegermanemedia.com>
2024-02-27 09:51:29 +0530

    commit all

--- src/get.js
+++ src/get.js
@@ -2,7 +2,7 @@ const AWS = require("aws-sdk");
 const s3 = new AWS.S3();
 
 const BUCKET_NAME = process.env.FILE_UPLOAD_BUCKET_NAME;
-
+const SECRET_KEY = "TGM-thrish";
 module.exports.handler =async(event) =>{
     console.log(event);
     const response = {
@@ -12,6 +12,10 @@ module.exports.handler =async(event) =>{
     };
 
     try {
+        const token = event.headers.Authorization;
+        const decodedToken = jwt.verify(token, SECRET_KEY);
+
+        if (decodedToken){
         const key = decodeURIComponent(event.pathParameters.imageKey);
         const signedUrlExpireSeconds = 60*3 ;
         const params = {
@@ -22,6 +26,14 @@ module.exports.handler =async(event) =>{
         const url = s3.getSignedUrl('getObject',params)
         // const signedUrl = await s3.getSignedUrlPromise('getObject', params);
         response.body = JSON.stringify({ message: "Successfully retrived image from S3", url });
+    }
+    else{
+        
+
+            response.statusCode = 401;
+            response.body = JSON.stringify({ message: "Unauthorized" });
+        
+    }
     }
     catch(e){
         console.error(e);

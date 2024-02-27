55239725488eefbec01994298528ba3835e8ea33
thrishaliniTGM <trishalini@thegermanemedia.com>
2024-02-28 00:52:57 +0530

    formatted

--- src/get.js
+++ src/get.js
@@ -4,43 +4,47 @@ const jwt = require("jsonwebtoken");
 
 const BUCKET_NAME = process.env.FILE_UPLOAD_BUCKET_NAME;
 const SECRET_KEY = "TGM-thrish";
-module.exports.handler =async(event) =>{
-    console.log(event);
-    const response = {
-        isBase64Encoded: false,
-        statusCode :200,
-        // Expires: 360
-    };
-
-    try {
-        const token = event.headers.Authorization;
-        const decodedToken = jwt.verify(token, SECRET_KEY);
-
-        if (decodedToken){
+module.exports.handler = async (event) => {
+  console.log(event);
+  const response = {
+    isBase64Encoded: false,
+    statusCode: 200,
+    // Expires: 360
+  };
+  try {
+    const token = event.headers.Authorization;
+    const decodedToken = jwt.verify(token, SECRET_KEY);
+    if (decodedToken) {
+      try {
         const key = decodeURIComponent(event.pathParameters.imageKey);
-        const signedUrlExpireSeconds = 60*3 ;
+        const signedUrlExpireSeconds = 60 * 3;
         const params = {
-            Bucket: BUCKET_NAME,
-            Key: key,
-            Expires: signedUrlExpireSeconds// URL expiration time in seconds (1 hour in this case)
+          Bucket: BUCKET_NAME,
+          Key: key,
+          Expires: signedUrlExpireSeconds,
         };
-        const url = s3.getSignedUrl('getObject',params)
+        const url = s3.getSignedUrl("getObject", params);
         // const signedUrl = await s3.getSignedUrlPromise('getObject', params);
-        response.body = JSON.stringify({ message: "Successfully retrived image from S3", url });
-        }
-        else{
-            
-
-                response.statusCode = 401;
-                response.body = JSON.stringify({ message: "Unauthorized" });
-            
-        }
-    }
-    catch(e){
+        response.body = JSON.stringify({
+          message: "Successfully retrived image from S3",
+          url,
+        });
+      } catch (e) {
         console.error(e);
-        response.body = JSON.stringify({message:"Error in file retrival from s3", errorMessage : e });
+        response.body = JSON.stringify({
+          message: "Error in file retrival from s3",
+          errorMessage: e,
+        });
         response.status = 500;
-    };
+      }
+    }
+  } catch (error) {
+    response.body = JSON.stringify({
+      message: "authentication fail",
+      error: error,
+    });
+    response.status = 401;
+  }
 
-    return response;
+  return response;
 };

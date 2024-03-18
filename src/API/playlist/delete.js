72342f43901f8a84b6ef95efcf08035a80cfe025
thrishaliniTGM <trishalini@thegermanemedia.com>
2024-03-18 12:40:33 +0530

    check

--- src/API/playlist/delete.js
+++ src/API/playlist/delete.js
@@ -2,7 +2,7 @@ const query = require('../../components/rdsConnection');
 
 module.exports.handler = async (event) => {
     try {
-        const programId = event.pathParameters.programId;
+        const programId = event.pathParameters.deleteProgramId;
 
         const result = await query(`DELETE FROM playlist WHERE programId = '${programId}'`);
 

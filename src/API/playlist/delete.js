004452c3f6cb646fd02992c0fc35117955fed212
thrishaliniTGM <trishalini@thegermanemedia.com>
2024-03-18 12:29:51 +0530

    delete from playlist added

---
+++ src/API/playlist/delete.js
@@ -0,0 +1,28 @@
+const query = require('../../components/rdsConnection');
+
+module.exports.handler = async (event) => {
+    try {
+        const programId = event.pathParameters.programId;
+
+        const result = await query(`DELETE FROM playlist WHERE programId = '${programId}'`);
+
+
+        if (result.affectedRows === 0) {
+            return {
+                statusCode: 404,
+                body: JSON.stringify({ message: "Program from playlist not found" })
+            };
+        }
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ message: "Program from playlist deleted successfully" })
+        };
+    } catch (error) {
+        console.error('Error deleting program from playlist', error);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: "Failed to delete program from playlist" })
+        };
+    }
+};
